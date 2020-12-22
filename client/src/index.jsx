587b18661ac70bae43bac69dee88704cbcb6d593
Matias Borghi <borghi.matias@gmail.com>
2020-12-22 16:51:03 -0300

    clean up html

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Editor from './Editor.jsx';
+// import Editor from './Editor.jsx';
+import Editor from './XTerm.jsx';
 
 render(<Editor />, document.getElementById('root'));
 
