d250bd7a0bd5d4d8b74bc67fa1d6487710a5356c
Matias Borghi <borghi.matias@gmail.com>
2021-02-24 10:27:49 -0300

    use nodemon for running the server

--- RUN.dev.sh
+++ RUN.dev.sh
@@ -1,5 +1,5 @@
 # RUN CLIENT
 gnome-terminal --tab --title="Client" -- bash -c "cd client; npm run start"
 
-# RUN FRONTEND
-gnome-terminal --tab --title="Server" -- bash -c "cd server; npm run start"
+# RUN SERVER
+gnome-terminal --tab --title="Server" -- bash -c "cd server; npm run watch"
